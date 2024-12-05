e7857ab0d0b99ea76cf14175673b88801e46e445
yousefsonny <130879965+yousefsonny@users.noreply.github.com>
2024-12-05 18:58:02 +0200

    Update 5_function_factorial.sh

--- 5_function_factorial.sh
+++ 5_function_factorial.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+new_func() {
+N=1
+echo "enter the number to give the factorial"
+read N=1
+factorail=1
+for ((i=1;i=<N;i++))
+do
+factorial=$((factorial*i))
+done
+echo "the factorial of the number $N is = $factorial"
+}
+new_func
